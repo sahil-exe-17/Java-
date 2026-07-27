2b182d609d694f50618187b5e3bdffb59ea007e7
Sahil <sahillale06@gmail.com>
2026-07-27 11:46:26 +0530

    Add Java program to calculate sum and check parity

---
+++ Experiment 1/Question 3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Question_3 {
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+
+        if (sum % 2 == 0) {
+            System.out.println("Sum is Even");
+        } else {
+            System.out.println("Sum is Odd");
+        }
+        sc.close();
+    }
+
+    }
